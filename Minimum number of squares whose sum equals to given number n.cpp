e3d50f35593a687ade4eff481696fc21853962f5
Ashis Kumar Singh <49238464+ashiscs@users.noreply.github.com>
2020-03-24 21:52:42 +0530

    Create Minimum number of squares whose sum equals to given number n.cpp

---
+++ Minimum number of squares whose sum equals to given number n.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int getMinSquares(int n)
+{
+    if(sqrt(n) - floor(sqrt(n)) == 0)
+    {
+        return 1;
+    }
+    if(n<=3)
+    {
+        return n;
+    }
+    
+    int res = n;
+    
+    for(int i=1; i<=n; i++)
+    {
+        int temp = i*i;
+        if(temp > n)
+        {
+            break;
+        }
+        else
+        {
+            res = min(res, 1 + getMinSquares(n - temp));
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the number\n";
+    cin>>n;
+    cout<<"\nMinimum number of square(s) required is: "<<getMinSquares(n);
+    return 0;
+}
+
+--------------------------------------------------------------------------------------------------
+
+/* OUTPUT
+
+Enter the number                                                     
+40                                                                   
+                                                                     
+Minimum number of square(s) required is: 2   (6^2 + 2^2 = 40)
+
+*/
